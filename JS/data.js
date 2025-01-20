6c9ec4bbe752beb66a91e614f859f069c6bc615a
SandraLlamasRizo <sandrallamasrizo@gmail.com>
2025-01-20 15:53:49 +0100

    interacciones carrito

--- JS/data.js
+++ JS/data.js
@@ -5,7 +5,7 @@ const productos = [
         imagen: 'https://mardemarbarcelona.com/cdn/shop/files/jersey-canale-punto-cuello-alto-color-azul_720x.jpg',
         descripcion: 'Jersey canalé en color azul, de cuello alto y ribete en el cuello.',
         precio: 19.99,
-        stock: 20
+        stock: 5
     },
     {
         id: 2,
@@ -13,15 +13,15 @@ const productos = [
         imagen: 'https://mardemarbarcelona.com/cdn/shop/files/jersey-canale-punto-cuello-alto-color-crema-2_1296x.jpg',
         descripcion: 'Jersey canalé en color crema, de cuello alto y ribete en el cuello.',
         precio: 19.99,
-        stock: 20
+        stock: 5
     },
     {
         id: 3,
         nombre: 'Camisa corazones',
         imagen: 'https://mardemarbarcelona.com/cdn/shop/files/blusa-blanca-bambula-corazones-bordados-6_720x.jpg',
-        descripcion: 'Camisa con corazones bordados',
+        descripcion: 'Camisa de lino con corazones bordados y manga larga.',
         precio: 29.99,
-        stock: 25
+        stock: 10
     },
     {
         id: 4,
@@ -29,47 +29,47 @@ const productos = [
         imagen: 'https://mardemarbarcelona.com/cdn/shop/files/chaleco-color-teja-botones-4_1296x.jpg',
         descripcion: 'Chaleco de cuello solapa y sin mangas. Cierre frontal con botones.',
         precio: 29.99,
-        stock: 20
+        stock: 4
     },
     {
         id: 5,
         nombre: 'Jersey gris perritos',
         imagen: 'https://mardemarbarcelona.com/cdn/shop/files/jersey-gris-claro-con-bordados-de-perritos-9_1296x.jpg',
-        descripcion: 'Jersey de color gris con puños y dobladillo acabados en canalé. Cuello redondo y manga larga, detalle de perritos bordados.',
+        descripcion: 'Jersey de color gris con puños y dobladillo acabados en canalé.',
         precio: 49.99,
-        stock: 50
+        stock: 10
     },
     {
         id: 6,
         nombre: 'Jersey beige perritos',
         imagen: 'https://mardemarbarcelona.com/cdn/shop/files/jersey-beige-con-bordados-de-perritos-5_1296x.jpg',
-        descripcion: 'Jersey de color beige con puños y dobladillo acabados en canalé. Cuello redondo y manga larga, detalle de perritos bordados.',
+        descripcion: 'Jersey de color beige con puños y dobladillo acabados en canalé.',
         precio: 49.99,
-        stock: 50
+        stock: 10
     },
     {
         id: 7,
         nombre: 'Jersey oversize corazones',
         imagen: 'https://mardemarbarcelona.com/cdn/shop/files/jersey-beige-con-corazones-bordados-3_540x.jpg',
-        descripcion: 'Jersey amplio de cuello vuelto y manga larga abullonada con corazones burdeos.',
+        descripcion: 'Jersey amplio de cuello vuelto y manga larga con corazones burdeos.',
         precio: 39.99,
-        stock: 50
+        stock: 5
     },
     {
         id: 8,
         nombre: 'Jersey rayas teja',
         imagen: 'https://mardemarbarcelona.com/cdn/shop/files/jersey-rayas-teja-brillantes-5_1296x.jpg',
-        descripcion: 'Jersey oversize con rayas teja y doradas. Hombros caido y fondo en color beige.',
+        descripcion: 'Jersey oversize con rayas teja y doradas.',
         precio: 33.99,
-        stock: 50
+        stock: 10
     },
     {
         id: 9,
         nombre: 'Camiseta negra brilli',
         imagen: 'https://mardemarbarcelona.com/cdn/shop/files/top-negro-brillantes-cuello-subido-6_1296x.jpg',
-        descripcion: 'Blusa tipo top de encaje con transparencias. Cuello perkins y color negro.',
+        descripcion: 'Blusa negra tipo top de encaje con transparencias. Cuello perkins.',
         precio: 14.99,
-        stock: 50
+        stock: 5
     },
     {
         id: 10,
@@ -77,7 +77,7 @@ const productos = [
         imagen: 'https://mardemarbarcelona.com/cdn/shop/files/blusa-asimetrica-brillantes-brilli-brilli-negra-5_1296x.jpg',
         descripcion: 'Top tipo blusa de un hombro en brillantes. Puño elástico.',
         precio: 26.99,
-        stock: 50
+        stock: 15
     },
     {
         id: 11,
@@ -85,7 +85,7 @@ const productos = [
         imagen: 'https://mardemarbarcelona.com/cdn/shop/files/jersey-oversize-ancho-beige-3_900x.jpg',
         descripcion: 'Jersey amplio de cuello vuelto y manga larga abullonada.',
         precio: 29.99,
-        stock: 50
+        stock: 5
     },
     {
         id: 12,
@@ -93,7 +93,7 @@ const productos = [
         imagen: 'https://mardemarbarcelona.com/cdn/shop/files/camiseta-canale-crema-blanco-2_1296x.jpg',
         descripcion: 'Camiseta de cuello redondo y manga larga. Cierre frontal con botones.',
         precio: 19.99,
-        stock: 50
+        stock: 10
     },
     {
         id: 13,
@@ -101,31 +101,31 @@ const productos = [
         imagen: 'https://mardemarbarcelona.com/cdn/shop/files/chaleco-beige-punto-8_900x.jpg',
         descripcion: 'Chaleco de punto beige cierre de botón.',
         precio: 29.99,
-        stock: 50
+        stock: 5
     },
     {
         id: 14,
-        nombre: 'Chaleco punto beige',
+        nombre: 'Chaleco punto burdeos',
         imagen: 'https://mardemarbarcelona.com/cdn/shop/files/chaleco-beige-punto-4_1296x.jpg',
         descripcion: 'Chaleco de punto burdeos cierre de botón.',
         precio: 29.99,
-        stock: 50
+        stock: 5
     },
     {
         id: 15,
         nombre: 'Chaqueta borreguito beige',
         imagen: 'https://mardemarbarcelona.com/cdn/shop/files/chaqueta-borreguito-beige-5_900x.jpg',
-        descripcion: 'Chaqueta de corte holgado en borreguito suave con cierre de botones.',
+        descripcion: 'Chaqueta de corte holgado en borreguito con cierre de botones.',
         precio: 49.99,
-        stock: 50
+        stock: 5
     },
     {
         id: 16,
         nombre: 'Chaqueta borreguito burdeos',
         imagen: 'https://mardemarbarcelona.com/cdn/shop/files/chaqueta-borreguito-granate-9_900x.jpg',
-        descripcion: 'Chaqueta de corte holgado en borreguito suave con cierre de botones.',
+        descripcion: 'Chaqueta de corte holgado en borreguito con cierre de botones.',
         precio: 49.99,
-        stock: 50
+        stock: 5
     },
     {
         id: 17,
@@ -133,15 +133,15 @@ const productos = [
         imagen: 'https://mardemarbarcelona.com/cdn/shop/files/chaleco-flores-bordadas-rosas-7_900x.jpg',
         descripcion: 'Chaleco color beige con flores rosas bordadas. Acabados en ondas.',
         precio: 33.99,
-        stock: 50
+        stock: 5
     },
     {
         id: 18,
         nombre: 'Jersey punto polo',
         imagen: 'https://mardemarbarcelona.com/cdn/shop/files/jersey-tipo-polo-color-crema-2_1296x.jpg',
-        descripcion: 'Jersey de punto. Manga larga, una ligera hombrera. Cuello tipo polo. Acabados en rib. Prenda ligera.',
+        descripcion: 'Jersey de punto. Manga larga, una ligera hombrera. Cuello tipo polo.',
         precio: 29.99,
-        stock: 50
+        stock: 10
     },
     {
         id: 19,
@@ -149,23 +149,23 @@ const productos = [
         imagen: 'https://mardemarbarcelona.com/cdn/shop/files/jersey-bardot-negro-6_1296x.jpg',
         descripcion: 'Jersey con cuello bardot. Hombros descubiertos y manga larga.',
         precio: 29.99,
-        stock: 50
+        stock: 5
     },
     {
         id: 20,
-        nombre: 'Cárdigan rosa corazones gris',
+        nombre: 'Cárdigan gris corazones rojos',
         imagen: 'https://mardemarbarcelona.com/cdn/shop/files/bolso-granate-asa-extrahible-3_900x.jpg',
-        descripcion: 'Cárdigan confeccionado con tejido de punto en mezcla de lana. Cuello redondo.',
+        descripcion: 'Cárdigan confeccionado con tejido de punto en mezcla de lana.',
         precio: 39.99,
-        stock: 50
+        stock: 5
     },
     {
         id: 21,
-        nombre: 'Cárdigan rosa corazones rosa',
+        nombre: 'Cárdigan rosa corazones rojos',
         imagen: 'https://mardemarbarcelona.com/cdn/shop/files/chaqueta-rosa-con-corazones-bordados-3_900x.jpg',
-        descripcion: 'Cárdigan confeccionado con tejido de punto en mezcla de lana. Cuello redondo.',
+        descripcion: 'Cárdigan confeccionado con tejido de punto en mezcla de lana.',
         precio: 39.99,
-        stock: 50
+        stock: 5
     },
     {
         id: 22,
@@ -173,15 +173,15 @@ const productos = [
         imagen: 'https://mardemarbarcelona.com/cdn/shop/files/jersey-estampado-naranja-y-marron-3_900x.jpg',
         descripcion: 'Jersey de cuello redondo y estampado de color marrón y naranja.',
         precio: 39.99,
-        stock: 50
+        stock: 5
     },
     {
         id: 23,
         nombre: 'Camisa verde',
         imagen: 'https://mardemarbarcelona.com/cdn/shop/files/camisa-verde-basica-solapa-y-cierre-botones_900x.jpg',
-        descripcion: 'Camisa verde básica',
+        descripcion: 'Camisa verde básica de lino y manga larga.',
         precio: 19.99,
-        stock: 50
+        stock: 10
     },
     {
         id: 24,
@@ -189,7 +189,7 @@ const productos = [
         imagen: 'https://mardemarbarcelona.com/cdn/shop/files/camisa-oversize-de-rayas-azul-oscuro-con-fondo-color-crema-11_900x.jpg',
         descripcion: 'Camisa oversized, con cuello de pico, botones delante.',
         precio: 29.99,
-        stock: 50
+        stock: 10
     },
     {
         id: 25,
@@ -197,30 +197,30 @@ const productos = [
         imagen: 'https://mardemarbarcelona.com/cdn/shop/files/pantalon-blanco-lino-tiro-alto-y-americana-lino-3-verde-militar_540x.jpg',
         descripcion: 'Americana con cuello solapa y manga larga. Color verde militar.',
         precio: 49.99,
-        stock: 50
+        stock: 5
     },
     {
         id: 26,
         nombre: 'Camiseta crema rayas',
         imagen: 'https://mardemarbarcelona.com/cdn/shop/files/short-negro-pantalon-corto-bermuda-lino-2copia_900x.jpg',
-        descripcion: 'Camiseta slim tipo Top cropped corto de canalé con rayas horizontales negras.',
+        descripcion: 'Camiseta slim tipo Top cropped corto de canalé a rayas.',
         precio: 24.99,
-        stock: 50
+        stock: 10
     },
     {
         id: 27,
         nombre: 'Camiseta puntilla azul',
         imagen: 'https://mardemarbarcelona.com/cdn/shop/files/camiseta-manga-corta-de-algodon-color-azul_900x.jpg',
-        descripcion: 'Camiseta confeccionada en hilatura de algodón 100%. Escote redondo y manga corta. Acabados en puntilla.',
+        descripcion: 'Camiseta confeccionada en hilatura de algodón 100%.',
         precio: 19.99,
-        stock: 50
+        stock: 10
     },
     {
         id: 28,
         nombre: 'Camiseta puntilla blanca',
         imagen: 'https://mardemarbarcelona.com/cdn/shop/files/camiseta-manga-corta-de-algodon-color-blanco-3_1296x.jpg',
-        descripcion: 'Camiseta confeccionada en hilatura de algodón 100%. Escote redondo y manga corta. Acabados en puntilla.',
+        descripcion: 'Camiseta confeccionada en hilatura de algodón 100%.',
         precio: 19.99,
-        stock: 50
+        stock: 10
     },
 ]
--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,12 @@
 //pintar menu carrito
-const carrito = document.querySelector('.js-carrito')
-const iconoCarrito = document.querySelector('.js-carrito-icono')
+const carrito = document.querySelector('.js-carrito');
+const iconoCarrito = document.querySelector('.js-carrito-icono');
+const sectionProductos = document.querySelector('.js-productosCarrito');
+const numeroCarrito = document.querySelector('.numero');
+const totalCarrito = document.querySelector('.js-total');
+const vaciarCarrito = document.querySelector('.js-vaciar');
+
+let carritoProductos = [];
 
 const menuCarrito = () => {
     const handleIconoCarrito = (event) => {
@@ -11,7 +17,154 @@ const menuCarrito = () => {
     iconoCarrito.addEventListener('click', handleIconoCarrito);
 }
 
+//handle botones cantidad carrito
+const handleMenos = (event) => {
+    const idProducto = parseFloat(event.target.name);
+    for (const item of carritoProductos){
+        if (item.id === idProducto && item.cantidad > 1){
+            item.cantidad --
+        } else if(item.id === idProducto && item.cantidad === 1){
+            const indice = carritoProductos.indexOf(item);
+            carritoProductos.splice(indice, 1);
+        }
+    }
+    pintarCarrito(carritoProductos);
+}
 
+const handleMas = (event) => {
+    const idProducto = parseFloat(event.target.name);
+    for (const item of carritoProductos){
+        if (item.id === idProducto){
+            if (item.cantidad < item.stock) {
+                item.cantidad ++
+            } else {
+                alert(`No queda stock de: ${item.nombre}`);
+            }
+        }
+    }
+    pintarCarrito(carritoProductos);
+}
+
+const handleEliminar = (event) => {
+    const idProducto = parseFloat(event.target.name);
+    for (const item of carritoProductos){
+        if (item.id === idProducto){
+            const indice = carritoProductos.indexOf(item);
+            carritoProductos.splice(indice, 1);
+        }
+    }
+    pintarCarrito(carritoProductos);
+}
+
+const handleVaciar = (event) => {
+    carritoProductos = [];
+    pintarCarrito(carritoProductos);
+}
+
+//calcular precio total y numero items
+
+const calcularTotal = (array) => {
+    let total = 0;
+    array.forEach((item) => total += (item.precio * item.cantidad))
+    return total;
+}
+
+const calcularNumeroItems = (array) => {
+    let total = 0;
+    array.forEach((item) => total +=  item.cantidad)
+    return total;
+}
+
+//pintar productos carrito
+
+
+
+const pintarUnCarrito = (producto) => {
+    const article = document.createElement('article');
+    article.classList.add('js-item');
+
+    const figure = document.createElement('figure');
+    figure.classList.add('imagenCarrito');
+    const img = document.createElement('img');
+    img.src = producto.imagen;
+    figure.appendChild(img);
+
+    const h3 = document.createElement('h3');
+    h3.classList.add('descripcion');
+    h3.textContent = producto.nombre;
+
+    const h6 = document.createElement('h6');
+    h6.classList.add('precioItem');
+    h6.textContent = `${producto.precio}€`
+
+    const divCantidad = document.createElement('div');
+    divCantidad.classList.add('cantidad');
+    const buttonMenos = document.createElement('button');
+    buttonMenos.classList.add('reducirCantidad');
+    buttonMenos.textContent = '-';
+    buttonMenos.setAttribute('name', producto.id);
+    buttonMenos.addEventListener('click', handleMenos);
+
+    const pCantidad = document.createElement('p');
+    pCantidad.textContent = producto.cantidad;
+    const buttonMas = document.createElement('button');
+    buttonMas.classList.add('augmentarCantidad');
+    buttonMas.textContent = '+';
+    buttonMas.setAttribute('name', producto.id);
+    buttonMas.addEventListener('click', handleMas);
+
+    const buttonEliminar = document.createElement('button');
+    buttonEliminar.classList.add('eliminar');
+    buttonEliminar.textContent = 'Eliminar';
+    buttonEliminar.setAttribute('name', producto.id);
+    buttonEliminar.addEventListener('click', handleEliminar);
+
+    divCantidad.appendChild(buttonMenos);
+    divCantidad.appendChild(pCantidad);
+    divCantidad.appendChild(buttonMas);
+    divCantidad.appendChild(buttonEliminar);
+
+    const ptotal = document.createElement('p');
+    const precioTotal = producto.precio * producto.cantidad;
+    ptotal.textContent = `${precioTotal.toFixed(2)}€`;
+
+    article.appendChild(figure);
+    article.appendChild(h3);
+    article.appendChild(h6);
+    article.appendChild(divCantidad);
+    article.appendChild(ptotal);
+
+    sectionProductos.appendChild(article);
+}
+
+const pintarCarrito = (array) => {
+    sectionProductos.innerHTML = '';
+    for (const producto of array){
+        pintarUnCarrito(producto);
+    };
+    const total = calcularTotal(carritoProductos);
+    totalCarrito.textContent = `Total: ${total.toFixed(2)} €`;
+    const totalItems = calcularNumeroItems(carritoProductos);
+    numeroCarrito.textContent = `(${totalItems})`;
+}
+
+//handle click añadirCarrito
+const handleAñadirCarrito = (event) => {
+    
+    const idProducto = parseFloat(event.target.name);
+    const nuevoProducto = productos.find(item => item.id === idProducto);
+    const indexCarrito = carritoProductos.findIndex(item => item.id === idProducto);
+    if (indexCarrito === -1){
+        nuevoProducto.cantidad = 1;
+        carritoProductos.push(nuevoProducto);
+    } else if (nuevoProducto.cantidad < nuevoProducto.stock){
+        nuevoProducto.cantidad ++
+    } else {
+        alert(`No queda stock de: ${nuevoProducto.nombre}`);
+    }
+    pintarCarrito(carritoProductos);
+
+}
 
 //pintar productos
 const gridProductos = document.querySelector('.js-productos')
@@ -34,11 +187,8 @@ const pintarUnProducto = (producto) => {
     const h6 = document.createElement('h6');
     h6.textContent = `${producto.precio} €`
     const button = document.createElement('button');
-    const i = document.createElement('i');
-    i.classList.add('fa-solid')
-    i.classList.add('fa-cart-shopping');
-    button.appendChild(i);
-    button.textContent = 'Añadir al carrito'
+    button.innerHTML = `<i class="fa-solid fa-cart-shopping"></i> Añadir al carrito `;
+    button.setAttribute('name', producto.id);
     div.appendChild(h3);
     div.appendChild(p);
     div.appendChild(h6);
@@ -46,20 +196,22 @@ const pintarUnProducto = (producto) => {
     section.appendChild(figure);
     section.appendChild(div);
     gridProductos.appendChild(section);
+
+    button.addEventListener('click', handleAñadirCarrito)
 }
 
-const pintarProductos = () => {
-    for (const producto of productos){
+const pintarProductos = (arrayProductos) => {
+    for (const producto of arrayProductos){
         pintarUnProducto(producto);
     }
 }
 
 
-
-
+//cargar DOM
 const initial = () => {
     menuCarrito();
-    pintarProductos();
+    pintarProductos(productos);
+    vaciarCarrito.addEventListener('click', handleVaciar);
 }
 
 initial();
